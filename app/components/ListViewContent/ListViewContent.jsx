d8d6f7ad05df412f64f4ed4bfc267f7c35d97083
Gongzhi Kan <kangongzhi@gmail.com>
2016-10-14 12:04:14 +1100

    updated ListViewContent component and test

--- app/components/ListViewContent/ListViewContent.jsx
+++ app/components/ListViewContent/ListViewContent.jsx
@@ -1,4 +1,24 @@
-import React  from 'react';
+import React, {PropTypes}  from 'react';
+import { List }  from 'immutable';
 
-const ListViewContent = () => (<div>{'content'}</div>);
+const ListViewContent = (props) => (<div>{props.cards.map((card) => {
+  let content = null;
+  switch (props.cardType) {
+    case 'account':
+      content = (<div className="account-card">account card template</div>);
+      break;
+    case 'contact':
+      content = (<div className="contact-card">contact card template</div>);
+      break;
+    default:
+      content = null;
+  }
+  return content;
+})}</div>);
+
+ListViewContent.propTypes = {
+  cards: PropTypes.instanceOf('List').isRequired,
+  cardType: PropTypes.string.isRequired,
+};
+
 export default ListViewContent;
--- tests/integration/components/ListViewContent-test.jsx
+++ tests/integration/components/ListViewContent-test.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ListViewContent } from 'components';
 import { expect } from 'chai';
+import { List } from 'immutable';
 
-const mockContent = 'content';
+const mockCardAccountType = 'account';
+const mockCardContactType = 'contact';
+const mockAccountCards = List([{}, {}, {}]);
+const mockContactCards = List([{}, {}, {}]);
 describe('<ListViewContent />', () => {
-  it('renders text content', () => {
-    const wrapper = shallow(<ListViewContent />);
-    expect(wrapper.text()).to.contain(mockContent);
+  it('renders text account cards', () => {
+    const wrapper = shallow(
+      <ListViewContent cardType={mockCardAccountType} cards={mockAccountCards} />);
+    expect(wrapper.find('.account-card')).to.have.length(3);
+  });
+  it('renders text contact cards', () => {
+    const wrapper = shallow(
+      <ListViewContent cardType={mockCardContactType} cards={mockContactCards} />);
+    expect(wrapper.find('.contact-card')).to.have.length(3);
   });
 });
